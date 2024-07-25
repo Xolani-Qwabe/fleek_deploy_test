ee661d611dce15b87b2b204b47bbe5f64cd0ddaa
xolani <thabisoxolaniqwabe@gmail.com>
2024-07-25 14:54:43 +0200

    animated text

--- src/App.jsx
+++ src/App.jsx
@@ -39,12 +39,13 @@ function App() {
         <div 
         id="intro-slider"
         className='h-screen p-10 bg-gray-50 absolute top-0 left-0 font-poppins z-10 w-full flex-col gap-10 tracking-tight '>
-          <h1 className='text-9xl' id="title-1">Drivers License</h1>
-          <h1 className='text-9xl' id="title-2">Learners License</h1>
-          <h1 className='text-9xl' id="title-3">PDP</h1>
+          <h1 className='text-4xl' id="title-1">Sbu's Driving School</h1>
+          <h1 className='text-4xl' id="title-2">Drivers License</h1>
+          <h1 className='text-4xl' id="title-3">Learners License</h1>
+          
         </div>
         <div className="h-screen flex bg-gray-950 justify-center place-items-center">
-          <h1 id="welcome" className='text-9xl font-bold text-gray-100 font-poppins'>Welcome To Sbu's Driving School</h1>
+          <h1 id="welcome" className='text-4xl font-bold text-gray-100 font-poppins'>Welcome</h1>
         </div>
       </div>
     </>
